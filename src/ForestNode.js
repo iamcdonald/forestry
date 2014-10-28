876e40e23e808506900d6926648d6e11c6edca50
Iain McDonald <iain.allan.mcdonald@googlemail.com>
2014-10-28 20:23:44 +0000

    Adding support for AMD, commonJS, global. Added .travis.yml for CI. Updated .gitignore

--- src/ForestNode.js
+++ src/ForestNode.js
@@ -1,85 +1,105 @@
-'use strict';
+(function (root, TreeNode) {
+	
+	if (typeof module !== 'undefined') {
+		module.exports = TreeNode; 
+		return;
+	} 
+	if (typeof define === 'function' && define.amd) {
+		define(function () {
+			return TreeNode;
+		});
+		return;
+	}
+	
+	root.TreeNode = TreeNode;
 
-function breadthFirstSearch(node, id) {
-	var arr = [],
-		found = null;
-	arr.push(node);
-	while (arr.length) {
-		node = arr.shift();
+}(this, (function () {
+
+	'use strict';
+
+	function breadthFirstSearch(node, id) {
+		var arr = [],
+			found = null;
+		arr.push(node);
+		while (arr.length) {
+			node = arr.shift();
+			if (node.id === id) {
+				found = node;
+				break;
+			}
+			arr = arr.concat(node.children);
+		}
+		return found;
+	}
+
+	function depthFirstSearch(node, id) {
+		var found = null;
 		if (node.id === id) {
-			found = node;
-			break;
+			return node;
 		}
-		arr = arr.concat(node.children);
+		for (var i = 0,length = node.children.length; i < length; i++) {
+			found = depthFirstSearch(node.children[i], id);
+			if (found) {
+				break;
+			}
+		}
+		return found;
 	}
-	return found;
-}
 
-function depthFirstSearch(node, id) {
-	var found = null;
-	if (node.id === id) {
+	var TreeNode = function (id, value) {
+		this.id = id;
+		this.children = [];
+		this.parent = null;
+		this.value = value;
+	};
+
+	TreeNode.prototype.getRoot = function () {
+		var node = this;
+		while (node.parent) {
+			node = node.parent;
+		}
 		return node;
-	}
-	for (var i = 0,length = node.children.length; i < length; i++) {
-		found = depthFirstSearch(node.children[i], id);
-		if (found) {
-			break;
-		}
-	}
-	return found;
-}
+	};
 
-var TreeNode = function (id, value) {
-	this.id = id;
-	this.children = [];
-	this.parent = null;
-	this.value = value;
-};
-
-TreeNode.prototype.getRoot = function () {
-	var node = this;
-	while (node.parent) {
-		node = node.parent;
-	}
-	return node;
-};
-
-TreeNode.prototype.addChild = function (node) {
-	node.parent = this;
-	this.children.push(node);
-	return this;
-};
-
-TreeNode.prototype.findNodeById = function (id, BFS) {	
-	if (BFS) {
-		return breadthFirstSearch(this, id);
-	}
-	return depthFirstSearch(this, id);
-};
-
-TreeNode.prototype.map = function (func) {
-	if (typeof func !== 'function') {
+	TreeNode.prototype.addChild = function (node) {
+		node.parent = this;
+		this.children.push(node);
 		return this;
-	}
-	var copy = new TreeNode(this.id, this.value);
-	copy.parent = this.parent;
+	};
 
-	for (var i = 0, length = this.children.length; i < length; i++) {
-		copy.children.push(this.children[i].map(func));
-	}
-	return func(copy);
-};
+	TreeNode.prototype.findNodeById = function (id, BFS) {	
+		if (BFS) {
+			return breadthFirstSearch(this, id);
+		}
+		return depthFirstSearch(this, id);
+	};
 
-TreeNode.prototype.reduce = function (acc, func) {
-	var node = this,
-		arr = [];
-	arr.push(node);
-	while (arr.length) {
-		node = arr.shift();
-		acc = func(acc, node);
-		arr = arr.concat(node.children);
-	}
-	return acc;
-};
+	TreeNode.prototype.map = function (func) {
+		if (typeof func !== 'function') {
+			return this;
+		}
+		var copy = new TreeNode(this.id, this.value);
+		copy.parent = this.parent;
 
-module.exports = TreeNode;
+		for (var i = 0, length = this.children.length; i < length; i++) {
+			copy.children.push(this.children[i].map(func));
+		}
+		return func(copy);
+	};
+
+	TreeNode.prototype.reduce = function (acc, func) {
+		var node = this,
+			arr = [];
+		arr.push(node);
+		while (arr.length) {
+			node = arr.shift();
+			acc = func(acc, node);
+			arr = arr.concat(node.children);
+		}
+		return acc;
+	};
+
+	return TreeNode;
+
+})()));
+
