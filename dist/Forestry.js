0afb9317f97316d085ebc284247ba722b0b28f17
Travis-CI <-->
2014-12-27 01:36:25 +0000

    Travis - Committing built assets

--- dist/Forestry.js
+++ dist/Forestry.js
@@ -2,14 +2,16 @@
 'use strict';
 
 var traversal = require('./traversal'),
-	Node = require('./Node');
+	Node = require('./Node'),
+	parse = require('./parse');
 
 module.exports = {
 	TRAVERSAL_TYPES: traversal.TYPES,
-	Node: Node
+	Node: Node,
+	parse: parse
 };
 
-},{"./Node":2,"./traversal":3}],2:[function(require,module,exports){
+},{"./Node":2,"./parse":3,"./traversal":4}],2:[function(require,module,exports){
 'use strict';
 
 var traversal = require('./traversal'),
@@ -56,14 +58,17 @@ Node.prototype.addChild = function (node) {
 };
 
 Node.prototype.remove = function () {
-	var children = this.parent.children;
-	this.parent.children = [];
-	for(var i = -1, len = children.length; ++i < len;) {
-		if (children[i] !== this) {
-			this.parent.children[this.parent.children.length] = children[i];
+	if (!this.isRoot()) {
+		var children = this.parent.children;
+		this.parent.children = [];
+		for(var i = -1, len = children.length; ++i < len;) {
+			if (children[i] !== this) {
+				this.parent.children[this.parent.children.length] = children[i];
+			}
 		}
+		this.parent = null;
 	}
-	return this;
+    return this;
 };
 
 Node.prototype.getRoot = function () {
@@ -144,7 +149,78 @@ Node.prototype.clone = function () {
 module.exports = Node;
 
 
-},{"./traversal":3}],3:[function(require,module,exports){
+},{"./traversal":4}],3:[function(require,module,exports){
+'use strict';
+
+var Node = require('./Node');
+
+function createNode(obj, childrenProp, valueProp) {
+	if (valueProp) {
+		return new Node(obj[valueProp]);
+	}
+	var data = {},
+		objKeys = Object.keys(obj);
+	for (var i = 0, len = objKeys.length; i < len; i++) {
+		if (objKeys[i] !== childrenProp) {
+			data[objKeys[i]] = obj[objKeys[i]];
+		}
+	}
+	return new Node(data); 
+}
+
+function asArray(children) {
+	if (children) {
+		if (children.constructor === Array) {
+			return children;
+		}
+		if (typeof children === 'object') {
+			var childrenArr = [],
+				keys = Object.keys(children);
+			for(var i = 0, l = keys.length; i < l; i++) {
+				if (children.hasOwnProperty(keys[i])) {
+					if (typeof children[keys[i]] !== 'object' || children[keys[i]].constructor === Array) {
+						throw new TypeError('Each child must be of type \'object\'');
+					}
+					childrenArr[i] = children[keys[i]];
+					childrenArr[i]._key = keys[i];
+				}
+			}
+			return childrenArr;
+		}
+		throw new TypeError('\'Children\' property can only be either an Object or Array');
+	}
+	return [];
+}
+
+function parse(obj, childrenProp, dataProp) {
+
+	if (typeof obj !== 'object' || obj.constructor === Array) {
+		throw new TypeError('Passed arg must be an object');
+	}
+	var rootNode = createNode(obj, childrenProp, dataProp),
+		arr = [[rootNode, obj]],
+		len = 1,
+		newNode,
+		childArr,
+		p;
+		
+	childrenProp = childrenProp || 'children';
+
+	while (len > 0) {
+		p = arr[--len];
+		childArr = asArray(p[1][childrenProp]);
+		for (var l = childArr.length; --l >= 0;) {
+			newNode = createNode(childArr[l], childrenProp, dataProp);
+			p[0].addChild(newNode);
+			arr[len++] = [newNode, childArr[l]];
+		}
+	}
+	return rootNode;
+}
+
+module.exports = parse;
+
+},{"./Node":2}],4:[function(require,module,exports){
 'use strict';
 
 function breadthFirstOp(node, op) {
