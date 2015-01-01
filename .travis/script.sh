0a4fa4898c11841edfd8c1ff48a9509e042a1148
Iain McDonald <iain.allan.mcdonald@googlemail.com>
2015-01-01 14:15:20 +0000

    Further addition to allow release tags without leading v

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,16 +4,19 @@ if [[ "${TRAVIS_BRANCH}" =~ ^v?[0-9]+\.[0-9]+\.[0-9]+$ ]]
 		echo "Publish";	
 		export BOWER_V=`sed -n 's/.*"version":.*\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/p' bower.json`;
 		export NPM_V=`sed -n 's/.*"version":.*\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/p' package.json`;
-		export TAG_V=`echo "${TRAVIS_BRANCH}" | sed -n 's/v?\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\)/\1/p'`;
+		export TAG_V=`echo "${TRAVIS_BRANCH}" | sed -n 's/v\{0,\}\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\)/\1/p'`;
 		if [[ ${TAG_V} == ${BOWER_V} && ${TAG_V} == ${NPM_V} ]] 
 			then
-				echo "Publishing package";
+				echo "Publishing package ${TAG_V}";
 				echo "//registry.npmjs.org/:_password=${NPM_TOKEN}" >> .npmrc
-				npm publish ./				
+				npm publish ./
+				echo "Success"
+			else
+				echo "Publishing package ${TAG_V} failed (versions not in alignment)"				
 		fi
 	else
 		echo "Build";
-  		npm test; 
+		npm test; 
   		export NAME="Travis-CI"
   		git config --global user.email "--"
   		git config --global user.name $NAME
