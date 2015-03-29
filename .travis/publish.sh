d431202a4ec03e56391f857e67748e88af519481
Travis-CI <-->
2015-03-29 22:06:54 +0100

    break build script into neater chunks

---
+++ .travis/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Publish";	
+export BOWER_V=`sed -n 's/.*"version":.*\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/p' bower.json`;
+export NPM_V=`sed -n 's/.*"version":.*\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\).*/\1/p' package.json`;
+export TAG_V=`echo "${TRAVIS_BRANCH}" | sed -n 's/v\{0,\}\([0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}\)/\1/p'`;
+if [[ ${TAG_V} == ${BOWER_V} && ${TAG_V} == ${NPM_V} ]] 
+	then
+		echo "Publishing package ${TAG_V}";
+		echo "//registry.npmjs.org/:_password=${NPM_TOKEN}" > ~/.npmrc
+		echo "//registry.npmjs.org/:email=iain.allan.mcdonald@googlemail.com" >> ~/.npmrc
+		npm publish ./
+		echo "Success"
+	else
+		echo "Publishing package ${TAG_V} failed (versions not in alignment)"				
+fi
+
