a6452d1691e431d35e65bba5fdb6da76a4a140d4
Iain McDonald <iain.allan.mcdonald@googlemail.com>
2015-04-08 18:58:30 +0100

    Moving script back to trigger either build or publish script

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-echo "//registry.npmjs.org/:_password=${NPM_TOKEN}" > ~/.npmrc
-echo "//registry.npmjs.org/:username=iamcdonald" >> ~/.npmrc
-echo "//registry.npmjs.org/:email=iain.allan.mcdonald@googlemail.com" >> ~/.npmrc
-
-npm whoami
-#if [[ "${TRAVIS_BRANCH}" =~ ^v?[0-9]+\.[0-9]+\.[0-9]+$ ]]
-# 	then 
-#		source .travis/publish.sh
-#	else
-#		source .travis/build.sh
-#fi
+if [[ "${TRAVIS_BRANCH}" =~ ^v?[0-9]+\.[0-9]+\.[0-9]+$ ]]
+ 	then 
+		source .travis/publish.sh
+	else
+		source .travis/build.sh
+fi
